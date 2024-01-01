a1c27c4414000ebd04222844f8df77e9af3ab728
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-01 22:26:14 +0600

    login page update : form  validation added (reactg hook form )

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,12 +4,14 @@ const Login = () => {
   const {
     register,
     handleSubmit,
-    watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
 
-  const handleLogin = () => {
-    console.log("login click");
+  const handleLogin = (data) => {
+    // console.log("login click");
+    // console.log(data);
+    const userEmail = data?.email;
+    const userPassword = data?.password;
   };
 
   return (
@@ -24,21 +26,72 @@ const Login = () => {
             onSubmit={handleSubmit(handleLogin)}
             className=" w-[92%] xsm:w-[80%] sm:w-[76%] md:w-[72%] m-auto flex flex-col gap-8  "
           >
-            <input
-              type="email"
-              id="email"
-              className="block w-full m-auto bg-gray-50  border border-gray-300 text-gray-900 text-sm rounded   p-2.5 outline-none "
-              placeholder="Enter your email"
-            />
-            <input
-              type="password"
-              id="password"
-              className="block w-full m-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded   p-2.5 outline-none "
-              placeholder="Enter your password"
-            />
+            {/* email input  */}
+            <div className="emailInput">
+              <input
+                type="email"
+                id="email"
+                {...register("email", {
+                  required: "Email is required",
+                })}
+                className={`block w-full m-auto  border bg-gray-50 border-gray-300     text-gray-900 text-sm rounded   p-2.5 outline-none`}
+                placeholder="Enter your email"
+              />
 
-            <button className=" w-full  py-2 rounded  bg-sky-500 hover:bg-sky-600 navLinkFont text-gray-50 font-medium  text-lg ">
-              Log in
+              {errors?.email && (
+                <p className=" pt-1.5 text-red-600 font-semibold ">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            {/* email input  */}
+
+            {/* password input  */}
+            <div className="passwordInput">
+              <input
+                type="password"
+                id="password"
+                {...register("password", {
+                  required: "Password is required",
+                })}
+                className="block w-full m-auto bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded   p-2.5 outline-none "
+                placeholder="Enter your password"
+              />
+              {errors?.password && (
+                <p className=" pt-1.5 text-red-600 font-semibold ">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            {/* password input  */}
+
+            <button
+              disabled={isSubmitting}
+              className=" w-full  py-2 rounded  bg-sky-500 hover:bg-sky-600 navLinkFont text-gray-50 font-medium  text-lg "
+            >
+              {isSubmitting ? (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
+                </div>
+              ) : (
+                "Log in"
+              )}
             </button>
 
             <button class="flex items-center justify-center gap-2 rounded border border-gray-300 bg-gray-100 px-8 py-3 text-center text-sm font-semibold text-gray-800 outline-none ring-gray-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:text-base">
